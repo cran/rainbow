b304427cd9f2c584c6510cf4600e128a89dca814
Han Lin Shang <HanLin.Shang@buseco.monash.edu.au>
2010-08-05 00:00:00 +0000

    version 2.1

--- R/SVDplot.R
+++ R/SVDplot.R
@@ -1,87 +1,44 @@
-SVDplot <-
-function(object, order = 1, center = c("rowwise", "colwise", "double"), plot = TRUE)
+SVDplot = function(object, order = 3, plot = TRUE, plot.type = c("fts", "image"), mfrow = c(2,3))
 {
-  center = match.arg(center)
   data = object$y
   p = dim(data)[1]
   n = dim(data)[2]
-  leftoutobs = order + 1
-  datarowmean = matrix(, p, (n - leftoutobs))
-  datacolmean = matrix(, (p - leftoutobs), n)
-  svdrow = array(, dim = c(p, (n - leftoutobs), order))
-  svdcol = array(, dim = c((p - leftoutobs), n, order)) 
-  if (center == "rowwise"){
-      newdata = data - rowMeans(data)
-  }
-  if (center == "colwise"){
-      newdata = t(t(data) - colMeans(data))
-  }
-  if (center == "double"){
-      newdata = t(t(data - rowMeans(data)) - colMeans(data)) + mean(data)
-  }
-  means = data - newdata
-  for(i in 1:(n - leftoutobs)){
-      datarowmean[,i] = as.matrix(rowMeans(data[, 1:(leftoutobs + i)]))
-  }
-  for(i in 1:(p - leftoutobs)){
-         datacolmean[i,] = as.matrix(colMeans(data[1:(leftoutobs + i), ]))
-  }
-  if (order > 0){
-      for(i in 1:(n - leftoutobs)){
-          for(j in 1:order){
-              svdrow[,i,j] = abs(svd(t(newdata)[1:(leftoutobs + i), ])$v[,j])
-          }
-      }
-      for(i in 1:(p - leftoutobs)){
-          for(j in 1:order){
-              svdcol[i,,j] = svd(t(newdata)[, 1:(leftoutobs + i)])$u[,j]
-          }
-      }
-  }
-  if (order == 1){
-      approx = as.matrix(svd(t(newdata))$v[,1]) %*% (svd(t(newdata))$d[1]*svd(t(newdata))$u[,1]) + means
-      resi = newdata - as.matrix(svd(t(newdata))$v[,1]) %*% (svd(t(newdata))$d[1]*svd(t(newdata))$u[,1])
-  }
-  if (order > 1){
-      svdcomb = array(, dim = c(p, n, order))
-      for(i in 1:order){
-          svdcomb[,,i] = as.matrix(svd(t(newdata))$v[,i]) %*% (svd(t(newdata))$d[i]*svd(t(newdata))$u[,i])
-      }
-      approx = apply(svdcomb, 1:2, sum) + means
-      resi = newdata - apply(svdcomb, 1:2, sum)
-  }
+  pcscore = svd(t(data))$u[,1:order]
+  pc = svd(t(data))$v[,1:order]
+  d = svd(t(data))$d[1:order]
+  svdr = array(, dim = c(p, n, order))
   if (plot == TRUE){
-      old.par <- par(no.readonly = TRUE)
-      if(order == 0){
-         par(mfrow = c(1,2))
-         plot(fts(as.numeric(rownames(object$y)), datarowmean), xlab = object$xname, ylab = "SVD1")
-         plot(fts(as.numeric(colnames(object$y)), t(datacolmean)), xlab = "Time", ylab = "SVD1")
+      plot.type = match.arg(plot.type)
+      xlab = object$xname
+      par(mfrow = mfrow)
+      if(plot.type == "fts"){
+         plot(object, main = "Original data")
+         for(i in 1:order){
+             svdr[,,i] = as.matrix(pc[,i] * d[i]) %*% t(pcscore[,i])
+             plot(fts(as.numeric(rownames(data)), svdr[,,i]),ylab = "",xlab=xlab, main = paste(expression(SVD), i, sep=""))
+         }
+         svdrecon = apply(svdr,c(1,2),sum)
+         resi = data - svdrecon
+         plot(fts(as.numeric(rownames(data)), svdrecon), ylab ="",xlab=xlab, main = "Reconstruction")
+         plot(fts(as.numeric(rownames(data)), resi), ylab ="",xlab=xlab, main = "Residual")
       }
       else{
-         par(mfrow = c(2, order + 3))
-         plot(fts(as.numeric(rownames(object$y)), datarowmean), xlab = object$xname, ylab = "SVD1")
+         image(as.numeric(rownames(data)), as.numeric(colnames(data)), data, main = "Original data", xlab = xlab, ylab = object$yname) 
+         box()
          for(i in 1:order){
-             plot(fts(as.numeric(rownames(object$y)), svdrow[,,i]), xlab = object$xname, ylab = paste("SVD",(i+1), sep = ""))
+             svdr[,,i] = as.matrix(pc[,i] * d[i]) %*% t(pcscore[,i])
+             image(as.numeric(rownames(data)), as.numeric(colnames(data)), svdr[,,i],ylab = "",xlab=xlab, main = paste(expression(SVD), i, sep=""))
+             box()
          }
-         plot(fts(as.numeric(rownames(object$y)), approx), xlab = object$xname, ylab = "SVD approximation")
-         plot(fts(as.numeric(rownames(object$y)), resi), xlab = object$xname, ylab = "Residual")
-         plot(fts(as.numeric(colnames(object$y)), t(datacolmean)), xlab = "Time", ylab = "SVD1")
-         for(i in 1:order){
-             plot(fts(as.numeric(colnames(object$y)), t(svdcol[,,i])), xlab = "Time", ylab = paste("SVD",(i+1), sep = ""))
-         }
-         plot(fts(as.numeric(colnames(object$y)), t(approx)), xlab = "Time", ylab = "SVD approximation")
-         plot(fts(as.numeric(colnames(object$y)), t(resi)), xlab = "Time", ylab = "Residual")
+         svdrecon = apply(svdr,c(1,2),sum)
+         resi = data - svdrecon
+         image(as.numeric(rownames(data)), as.numeric(colnames(data)), svdrecon, ylab = "", xlab = xlab, main = "Reconstruction")
+         box()
+         image(as.numeric(rownames(data)), as.numeric(colnames(data)), resi, ylab = "", xlab = xlab, main = "Residual")        
+         box()
       }
-      on.exit(par(old.par)) 
   }
-  else {
-       if (order == 0){
-           return(list(datarowmean = datarowmean, datacolmean = datacolmean, xname = object$xname, yname = object$yname))
-       }
-       else{
-           return(list(datarowmean = datarowmean, datacolmean = datacolmean, svdrow = svdrow, svdcol = svdcol,
-                  approx = approx, residual = resi, xname = object$xname, yname = object$yname))
-       }
+  else{
+      return(list(svds = svdr, reconstruction = svdrecon, residual = resi))
   }
 }
-
--- R/plot.fds.R
+++ R/plot.fds.R
@@ -1,5 +1,5 @@
 `plot.fds` <- function (x, plot.type = c("functions", "time", "depth", "density"), labels = NULL, label.cex = 0.7, 
-        col = NULL, type = "l", lty = 1, xlab = x$xname, ylab = x$yname, pch = c(1:9,0, letters, LETTERS), ...)
+        col = NULL, type = "l", lty = 1, xlab = x$xname, ylab = x$yname, pch = c(1:9,0, letters, LETTERS), add = FALSE, index, ...)
 {
     if (class(x)[1] == "fts"|class(x)[1] == "fds"|class(x)[1] == "sfts"){
         plot.type <- match.arg(plot.type)    
@@ -14,8 +14,15 @@
                     xlab <- "Time"
                 }
                 year = as.numeric(colnames(x$y))
-                matplot(year, t(x$y), type = type, ylab = ylab, 
-                        xlab = xlab, col = col, lty = lty, pch = pch, ...)
+				if (add == FALSE){
+                    matplot(year, t(x$y), type = type, ylab = ylab, 
+                            xlab = xlab, col = col, lty = lty, pch = pch, ...)
+				}
+				else
+				{
+				    matlines(year, t(x$y)[,index], type = type, ylab = ylab, 
+					        xlab = xlab, col = col, lty = lty, pch = pch, ...)
+				}
             }
             else{
                 stop("object is not a functional time series.")
@@ -32,8 +39,8 @@
             if (plot.type == "depth"){
                 sco <- PCAproj(t(yy), k = 2)$score
                 center <- compute.bagplot(sco)$center
-                index <- order(mahalanobis(sco, center, cov(sco)))
-                yy <- yy[,index]
+                lineindex <- order(mahalanobis(sco, center, cov(sco)))
+                yy <- yy[,lineindex]
                 yymax <- yy[,1]
             }   
             else if (plot.type == "density"){
@@ -43,19 +50,29 @@
                      den = kde(x = X, H = h)
                      den = list(x = den$eval.points[[1]], y = den$eval.points[[2]], z = den$estimate)
                      den2 <- hdrcde:::hdr.info.2d(sco[,1], sco[,2], den, alpha = c(0.01,.5))
-                     index <- order(den2$fxy, decreasing = TRUE)
-                     yy <- yy[,index]
+                     lineindex <- order(den2$fxy, decreasing = TRUE)
+                     yy <- yy[,lineindex]
                      yymax <- yy[,1]
             }
             if (nrow(yy) == 1)
                 plot(ts(c(yy), s = start(x$time), f = frequency(x$time)),
                      ylab = ylab, ...)        
             if (plot.type == "functions"){
-                matplot(x$x, yy, col = col, xlab = xlab, ylab = ylab, type = type, lty = lty, pch = pch, ...)
+			    if (add == FALSE){
+                    matplot(x$x, yy, col = col, xlab = xlab, ylab = ylab, type = type, lty = lty, pch = pch, ...)
+				}
+                else{
+                    matlines(x$x, yy[,index], col = col, xlab = xlab, ylab = ylab, type = type, lty = lty, pch = pch, ...)				    
+                }				
             }
             else{      
-                matplot(x$x, yy, col = col, xlab = xlab, ylab = ylab, type = type, lty = lty, pch = pch, ...)
-                lines(x$x, yymax, col = "black", ...)
+			    if (add == FALSE){
+                    matplot(x$x, yy, col = col, xlab = xlab, ylab = ylab, type = type, lty = lty, pch = pch, ...)
+                    lines(x$x, yymax, col = "black", ...)
+				}
+				else{
+				    matlines(x$x, yy[,index], col = col, xlab = xlab, ylab = ylab, type = type, lty = lty, pch = pch, ...)
+				}
         }
         palette("default") 
         }
